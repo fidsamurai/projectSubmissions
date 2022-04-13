74f5453b93c4c60b3398fed17db07ce2bfdaca1a
fidsamurai <firdausbeckham23@gmail.com>
2022-04-13 20:02:17 +0530

    Terraform project

---
+++ terraform.tf
@@ -0,0 +1,55 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_security_group" "SSH" {
+  name = "SSH Fid Home"
+  vpc = aws_vpc.main.id
+
+  ingress {
+    description = "SSH Fid"
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  tags = {
+    Name = "devops"
+  }
+}
+
+resource "aws_instance" "pgpDemo" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3a.micro"
+  key_name = "devops"
+  vpc_security_group_ids = aws_security_group.SSH.id
+
+  user_data = "#!/bin/bash
+    sudo apt update && sudo apt install openjdk-11-jdk -y
+    sudo apt install python3 python3-pip
+    curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install jenkins" 
+
+  tags = {
+    Name = "pgpDevops"
+    Env = "pgpDevops"
+  }
+}
