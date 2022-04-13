ea3d1a41739f1d46582e01e257bd8cf96578438c
fidsamurai <firdausbeckham23@gmail.com>
2022-04-13 20:11:46 +0530

    Terraform project - HereDoc UserData

--- terraform.tf
+++ terraform.tf
@@ -37,16 +37,18 @@ resource "aws_instance" "pgpDemo" {
   key_name = "devops"
   vpc_security_group_ids = aws_security_group.SSH.id
 
-  user_data = "#!/bin/bash \
-    sudo apt update && sudo apt install openjdk-11-jdk -y \
-    sudo apt install python3 python3-pip \
-    curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  user_data = "EOD
+    #!/bin/bash
+    sudo apt update && sudo apt install openjdk-11-jdk -y
+    sudo apt install python3 python3-pip
+    curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee
     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-    /etc/apt/sources.list.d/jenkins.list > /dev/null \
-    sudo apt-get update \
-    sudo apt-get install jenkins"
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install jenkins
+    EOD"
 
   tags = {
     Name = "pgpDevops"
