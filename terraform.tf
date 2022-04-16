2d876e7876d571592ec10a2ade698d666bfb84ed
fidsamurai <firdausbeckham23@gmail.com>
2022-04-16 14:22:40 +0530

    Added default egress rule for security group

--- terraform.tf
+++ terraform.tf
@@ -64,6 +64,21 @@ resource "aws_security_group" "SSH" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "jenkins Fid"
+    from_port = "8080"
+    to_port = "8080"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
     Name = "devops"
