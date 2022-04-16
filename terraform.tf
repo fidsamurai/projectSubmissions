fa95871c2109ec4e74bd38ac00e89c87b4a6cdd4
fidsamurai <firdausbeckham23@gmail.com>
2022-04-16 13:18:42 +0530

    Added IGW and IGW attachment

--- terraform.tf
+++ terraform.tf
@@ -33,7 +33,7 @@ resource "aws_subnet" "devops" {
 }
 
 resource "aws_internet_gateway" "devops" {
-    vpc_id = aws.vpc.devops.id
+    vpc_id = aws_vpc.devops.id
 
     tags {
       Name = "devops"
